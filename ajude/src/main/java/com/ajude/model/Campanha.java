0a1580213fb5b055923a8bb101fed1b2cbe27978
117210367 CAIO MAXXIMUS PEREIRA SOARES <caio.soares@ccc.ufcg.edu.br>
2019-10-25 11:28:16 -0300

    atributos campanha e metodo para construir url

--- ajude/src/main/java/com/ajude/model/Campanha.java
+++ ajude/src/main/java/com/ajude/model/Campanha.java
@@ -3,14 +3,17 @@ package com.ajude.model;
 import sun.util.calendar.BaseCalendar;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.Date;
 import java.util.List;
+import java.text.Normalizer;
+import java.util.regex.Pattern;
 
 @Entity
 public class Campanha {
 
-    @Id
+    @Id @GeneratedValue
     private int id;
     private String nomeCurto;
     //Gerado pelo frontend
@@ -24,4 +27,23 @@ public class Campanha {
     private Usuario dono;
     private List<Comentario> comentarios;
     private List<Like> likes;
+
+    public Campanha(){
+        super();
+    }
+
+    public  Campanha(String nomeCurto,String descricao, String deadLine,Double meta,Usuario dono){
+        this.nomeCurto = nomeCurto;
+        this.descricao = descricao;
+        this.meta = meta;
+        this.dono = dono;
+        this.url = makeUrl(this.nomeCurto);
+    }
+
+
+    private static String makeUrl(String str) {
+        String nfdNormalizedString = Normalizer.normalize(str, Normalizer.Form.NFD);
+        Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+        return pattern.matcher(nfdNormalizedString).replaceAll("").replace(" ","-").replaceAll("[!#$%&'()*+,.:;?@[\\\\]_`{|}~]","").toLowerCase();
+    }
 }
--- ajude/src/main/java/com/ajude/model/Like.java
+++ ajude/src/main/java/com/ajude/model/Like.java
@@ -1,4 +1,11 @@
 package com.ajude.model;
 
 public class Like {
+
+    private Usuario user;
+
+    public Like(Usuario user){
+        this.user = user;
+    }
+
 }

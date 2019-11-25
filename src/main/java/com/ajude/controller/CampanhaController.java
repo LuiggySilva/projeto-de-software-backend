4f7053beab98fed79193e2b876ea16520181edc2
Luiggy Silva <38479645+LuiggySilva@users.noreply.github.com>
2019-11-25 09:32:51 -0300

    Update CampanhaController.java

--- src/main/java/com/ajude/controller/CampanhaController.java
+++ src/main/java/com/ajude/controller/CampanhaController.java
@@ -90,7 +90,7 @@ public class CampanhaController {
 	public ResponseEntity<Campanha> responderComentario(@PathVariable long id,@RequestHeader("Authorization") String header, @RequestBody Comentario comentario){
 		Campanha resul = campanhaService.responderComentarioCampanha(id, comentario, header);
 		if(resul != null) {
-			return new ResponseEntity<Campanha>(HttpStatus.OK);
+			return new ResponseEntity<Campanha>(resul, HttpStatus.OK);
 		}
 		else {
 			return new ResponseEntity<Campanha>(HttpStatus.UNAUTHORIZED);
